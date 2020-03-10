5f7bafa72eccfe754d4a439c88456d50d8a5639e
Pappi W <karite.wanjohi@gmail.com>
2020-03-10 17:37:31 +0300

    Initial commit

---
+++ app/src/main/java/ke/co/droidsense/primechecker/Solution.java
@@ -0,0 +1,17 @@
+package ke.co.droidsense.primechecker;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.lang.System.in;
+
+
+
+
+
+
+
+
